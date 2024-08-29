ca59b43c92aa0a6d9a2bacd63026b76c9f8e4887
Marc Pangan <67208427+mrcpngn@users.noreply.github.com>
2024-08-29 15:37:50 +0800

    optimize by auto getting the jumphost config

--- ssh_transfer.py
+++ ssh_transfer.py
@@ -1,90 +1,101 @@
 import paramiko
 import os
 import argparse
+from paramiko import SSHConfig
 from scp import SCPClient
 
-def load_ssh_config(hostname):
-    ssh_config_file = os.path.expanduser("~/.ssh/config")
-    ssh_config = paramiko.SSHConfig()
-    with open(ssh_config_file) as f:
+def load_ssh_config(private_host):
+    ssh_config = SSHConfig()
+    with open(os.path.expanduser('~/.ssh/config')) as f:
         ssh_config.parse(f)
-    return ssh_config.lookup(hostname)
+    
+    # Lookup the host in the SSH config
+    config = ssh_config.lookup(private_host)
+    return config
 
-def transfer_directory_via_bastion(target_host):
-    with SCPClient(target_host.get_transport()) as scp:
+def transfer_directory_via_bastion(private_host):
+    with SCPClient(private_host.get_transport()) as scp:
         scp.put(args.local_dir_path, args.remote_dir_path, recursive=True)
-        print(f"Successfully transferred {args.local_dir_path} to {args.remote_dir_path}")
-        
-def ssh_login_and_transfer_files(jumpbox_host_alias, jumpbox_key_passphrase, target_host_alias, target_key_passphrase):
+        print(f"INFO: Successfully transferred {args.local_dir_path} to {args.remote_dir_path}")
+
+def ssh_to_private_server(private_host, private_passphrase, jumpbox_passphrase):
     try:
-        # Load the SSH Config for the Jumpbox
-        jumpbox_config = load_ssh_config(jumpbox_host_alias)
+        # Load the SSH config for the private host
+        config = load_ssh_config(private_host)
 
-        # Retrieve connection details from SSH config
-        jumpbox_hostname = jumpbox_config.get('hostname')
-        jumpbox_username = jumpbox_config.get('user')
-        jumpbox_key_file = os.path.expanduser(jumpbox_config.get('identityfile')[0]) if 'identityfile' in jumpbox_config else None
-        jumpbox_port = int(jumpbox_config.get('port', 22))
+        if 'proxyjump' in config:
+            # Load the Jumpbox SSH config
+            jumpbox = config['proxyjump']
+            jumpbox_config = load_ssh_config(jumpbox)
+    
+            # Create an SSH client for the jumpbox host
+            jumpbox_client = paramiko.SSHClient()
+            jumpbox_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-        # Load the SSH Config for the target host
-        target_config = load_ssh_config(target_host_alias)
+            # Connect to  the private server using SSH config settings
+            jumpbox_client.connect(
+                hostname=jumpbox_config['hostname'],
+                username=jumpbox_config.get('user'),
+                key_filename=jumpbox_config.get('identityfile'),
+                passphrase=jumpbox_passphrase,
+            )
 
-        # Retrieve connection details from SSH config
-        target_hostname = target_config.get('hostname')
-        target_username = target_config.get('user')
-        target_key_file = os.path.expanduser(target_config.get('identityfile')[0]) if 'identityfile' in target_config else None
-        target_port = int(target_config.get('port', 22))
+            jumpbox_transport = jumpbox_client.get_transport()
+            # Get the private address of the Jumpbox
+            stdin, stdout, stderr = jumpbox_client.exec_command('hostname -I | awk \'{print $1}\'')
+            src_addr = (stdout.read().decode().strip(), 22)
+            dest_addr = (config['hostname'], 22)
+            jumpbox_channel = jumpbox_transport.open_channel("direct-tcpip",dest_addr, src_addr )
 
-        # Create the Jumpbox SSH client
-        jumpbox = paramiko.SSHClient()
-        jumpbox.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        else:
+            proxyjump = None
 
-        # Load the private key with the passphrase
-        jumpbox_key = paramiko.RSAKey.from_private_key_file(jumpbox_key_file, password=jumpbox_key_passphrase)
-        target_key = paramiko.RSAKey.from_private_key_file(target_key_file, password=target_key_passphrase)
+        # Create an SSH client for the private host
+        private_client = paramiko.SSHClient()
+        private_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-        # Connect to the SSH server using the SSH config settings
-        jumpbox.connect(hostname=jumpbox_hostname, port=jumpbox_port, username=jumpbox_username, pkey=jumpbox_key)
-        jumpbox_transport = jumpbox.get_transport()
 
-        # Get the private address of the Jumpbox
-        stdin, stdout, stderr = jumpbox.exec_command('hostname -I | awk \'{print $1}\'')
-        src_addr = (stdout.read().decode().strip(), 22)
-        dest_addr = (target_hostname, 22)
-        jumpbox_channel = jumpbox_transport.open_channel("direct-tcpip",dest_addr, src_addr )
+        # Connect to  the private server using SSH config settings
+        private_client.connect(
+            hostname=config['hostname'],
+            username=config.get('user'),
+            key_filename=config.get('identityfile'),
+            passphrase=private_passphrase,
+            sock=jumpbox_channel
+        )
 
-        # Create the taret host SSH client
-        target = paramiko.SSHClient()
-        target.set_missing_host_key_policy(paramiko.AutoAddPolicy())    
-        target.connect(hostname=target_hostname, port=target_port, username=target_username, pkey=target_key, sock=jumpbox_channel)
+        # Execute a command on the private server
+        stdin, stdout, stderr = private_client.exec_command('hostname')
+        print(f"INFO: Successfully connected to {(stdout.read().decode().strip())}")
 
-        # Transfer the file to the target host
-        transfer_directory_via_bastion(target)
+        # Transfer the file to the private host
+        transfer_directory_via_bastion(private_client)
 
         # Close the connection
-        target.close
-        jumpbox.close()
+        private_client.close()
+        jumpbox_client.close
     
     except paramiko.AuthenticationException:
-        print("Authentication failed. Please check your credentials.")
+        print("ERROR: Authentication failed. Please check your credentials.")
     except Exception as e:
-        print(f"An error occurred: {e}")
+        print(f"ERROR: An error occured: {e}")
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
-    # Arguments
-    parser.add_argument("jumpbox_host_alias")       # Jumpbox host alias
-    parser.add_argument("jumpbox_key_passphrase")   # Jumpbox host passphrase
+    # File transfer arguments
+    parser.add_argument("local_dir_path") 
+    parser.add_argument("remote_dir_path")
 
-    parser.add_argument("target_host_alias")        # Target host alias
-    parser.add_argument("target_key_passphrase")    # Target host passphrase
+    # Private host arguments
+    parser.add_argument("private_host")
+    parser.add_argument("private_passphrase")
 
-    parser.add_argument("local_dir_path")  # Local directory to transfer
-    parser.add_argument("remote_dir_path")  # Destination directory on the remote server
+    # Jumpbox arguments
+    parser.add_argument("jumpbox_passphrase")
 
     args = parser.parse_args()
 
     # Pass the arguments to the function
-    ssh_login_and_transfer_files(args.jumpbox_host_alias, args.jumpbox_key_passphrase, args.target_host_alias, args.target_key_passphrase)
-    
+    ssh_to_private_server(args.private_host,args.private_passphrase,args.jumpbox_passphrase)
